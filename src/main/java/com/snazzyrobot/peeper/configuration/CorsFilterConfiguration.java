78c38d7f28a033e65bbb291e93fc4b6f68cafe9a
Eric Mulvihill <eric.mulvihill@gmail.com>
2024-12-10 10:45:20 -0500

    Added ability to post createVideoSnap from client to spring server.

--- src/main/java/com/snazzyrobot/peeper/configuration/CorsFilterConfiguration.java
+++ src/main/java/com/snazzyrobot/peeper/configuration/CorsFilterConfiguration.java
@@ -1,23 +1,47 @@
 package com.snazzyrobot.peeper.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-    @Configuration
-    public class CorsFilterConfiguration {
-        @Bean
-        public CorsFilter corsFilter() {
-            final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            final CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.addAllowedOrigin("http://localhost:5000");
-            config.addAllowedHeader("*");
-            config.addAllowedMethod("*");
-            source.registerCorsConfiguration("/graphql/**", config);
-            return new CorsFilter(source);
-        }
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsFilterConfiguration {
+
+    final Environment env;
+
+    public CorsFilterConfiguration(Environment env) {
+        this.env = env;
     }
+
+    @Value("${spring.graphql.cors.allowed-origins}")
+    private String allowedOriginsEnv;
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        // String allowedOriginsEnv = env.getProperty("ALLOWED_ORIGINS");
+        List<String> allowedOrigins;
+
+        if (allowedOriginsEnv == null || allowedOriginsEnv.isEmpty()) {
+            allowedOrigins = List.of();
+        } else {
+            allowedOrigins = Arrays.asList(allowedOriginsEnv.split(","));
+        }
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/graphql/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/main/resources/db/migration/V1__.sql
+++ src/main/resources/db/migration/V1__.sql
@@ -3,6 +3,6 @@ CREATE TABLE video_snap
     id      BIGINT AUTO_INCREMENT NOT NULL,
     feed_id BIGINT                NOT NULL,
     date    DATETIME              NOT NULL,
-    data    VARCHAR(255)          NOT NULL,
+    data    TEXT                  NOT NULL,
     CONSTRAINT pk_video_snap PRIMARY KEY (id)
 );
